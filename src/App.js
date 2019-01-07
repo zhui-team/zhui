2cd00884bc1e1a11383f1d1847ae623f304231bf
AddOndDn <467072280@qq.com>
2019-01-08 00:40:54 +0800

    feature/button组件部分完善

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import { Button } from './components';
 
 class App extends Component {
+  myClick() {
+    console.log('click');
+  }
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{background: '#333', height: 100}}>
         <Button 
-          value='Hello My Btn'
-          width={200}
-          height={200}
-        ></Button>
+          onClick={this.myClick}
+        >aaaa</Button>
       </div>
     );
   }
