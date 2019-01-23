88d8b03b1e23cbc1566ea809b0df0aa474b550a8
AddOndDn <467072280@qq.com>
2019-01-23 17:55:20 +0800

    test: 补充input组件测试

--- src/__tests__/input.test.js
+++ src/__tests__/input.test.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Input from '../components/input/index'
+import Icon from '../components/icon/index'
 
 describe('Input Test', () => {
   const wrapper = shallow(
     <Input
-      size="large"
+      size='large'
       placeholder='Waiting...' 
+      theme='meihong'
+      addonBefore='A'
+      addonAfter='Z'
+      icon={<Icon type='search' />}
     />
   );
 
@@ -19,15 +24,37 @@ describe('Input Test', () => {
   it('2. 测试type存在', () => {
     expect(wrapper.hasClass('zhui-input-wrapper-large')).toBe(true);
   });
+
+  it('3. 测试theme存在', () => {
+    expect(wrapper.find('.zhui-input-meihong').exists()).toBeTruthy();
+  });
+
+  it('4. 测试前后缀存在', () => {
+    expect(wrapper.hasClass('zhui-input-group')).toBe(true);
+    expect(wrapper.first().text()).toBe('AZ');
+  });
+
+  it('5. 测试icon存在', () => {
+    let iconWrapper = shallow(
+      <Input icon={<Icon type='search' />} />
+    );
+
+    expect(iconWrapper.find('.zhui-input-icon-wrapper').exists()).toBeTruthy();
+    expect(iconWrapper.find('.zhui-input-icon-wrapper').length).toBe(1);
+  });
 });
 
 describe('Textarea Test', () => {
   const wrapper = shallow(
-    <Input.Textarea placeholder="搜索" />
+    <Input.Textarea placeholder='搜索' theme='ganglan'/>
   );
 
   it('1. 测试默认class存在', () => {
     expect(wrapper.hasClass('zhui-input-textarea-wrapper')).toBe(true);
     expect(wrapper.find('.zhui-input-textarea').exists()).toBeTruthy();
+  });
+
+  it('2. 测试theme存在', () => {
+    expect(wrapper.find('.zhui-input-ganglan').exists()).toBeTruthy();
   });
 });
