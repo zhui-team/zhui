8766a46c2ed9d4948d0428ef7257d17c7ea03a39
AddOndDn <467072280@qq.com>
2019-01-28 16:26:43 +0800

    feat: 完善部分progress组件

--- src/components/progress/Progress.js
+++ src/components/progress/Progress.js
@@ -12,6 +12,7 @@ export default class Progress extends PureComponent {
     type: PropTypes.oneOf(['line', 'circle']),
     status: PropTypes.oneOf(['active', 'exception']),
     precent: PropTypes.number,
+    theme: PropTypes.string,
     showInfo: PropTypes.bool
   }
 
@@ -21,6 +22,7 @@ export default class Progress extends PureComponent {
     type: 'line',
     status: 'active',
     precent: 0,
+    theme: 'meihong',
     showInfo: true
   }
 
@@ -30,22 +32,34 @@ export default class Progress extends PureComponent {
       prefix,
       type,
       precent,
+      status,
       showInfo,
+      theme,
       ...others
     } = this.props;
 
-    const classes = cn(className, `${prefix}-wrapper`);
+    const classes = cn(className, `${prefix}-wrapper`, {
+      [`${prefix}-${type}`]: type,
+    });
+    const innerClass = cn(`${prefix}-inner`, {
+      [`${prefix}-active`]: precent < 100 && status !== 'exception',
+      [`${prefix}-exception`]: status === 'exception',
+      [`${prefix}-success`]: precent >= 100,
+    });
+    const bgClass = cn(`${prefix}-bg`, {
+      [`${prefix}-bg-${theme}`]: theme && theme !== 'meihong'
+    });
     const width = precent + '%';
 
     return (
       <div className={classes} {...others}>
-        <div className={`${prefix}-inner`}>
-          <div className={`${prefix}-bg`} style={{width: width}}>
+        <div className={innerClass}>
+          <div className={bgClass} style={{ width: width }}>
           </div>
         </div>
-        <div className={`${prefix}-outer`}>
+        <span className={`${prefix}-outer`}>
           {showInfo && width}
-        </div>
+        </span>
       </div>
     );
   }
