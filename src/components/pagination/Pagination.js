ad25c2c9ce24ecb038c64d5165ac4dba66208700
AddOndDn <467072280@qq.com>
2019-02-02 20:18:53 +0800

    feat: 简单完善分页组件

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -23,8 +23,30 @@ export default class Switch extends PureComponent {
     onChange: () => null
   }
 
+  _onChange = e => {
+    const { onChange } = this.props;
+    let page = e.currentTarget.innerText;
+    onChange(page, e);
+  }
+
+  renderList = () => {
+    const { total, current } = this.props;
+    let list = [];
+    for(let i = 0 ; i < total ; i++) {
+      i + 1 === current ? 
+        list.push(
+          <span key={i} onClick={this._onChange} className='zhui-pagination-active'>{i + 1}</span>
+        ) :
+        list.push(
+          <span key={i} onClick={this._onChange}>{i + 1}</span>
+        )
+    }
+
+    return list;
+  }
+
   render() {
-    const {
+    let {
       className,
       prefix,
       current,
@@ -34,17 +56,22 @@ export default class Switch extends PureComponent {
       ...others
     } = this.props;
 
-    const classes = cn(className, prefix, {
-
-    });
+    const classes = cn(className, prefix);
 
     current = current < 1 ? 1 :
       current > total ? total : current;
 
     return (
       <div className={classes} {...others}>
-        pagination
+      {
+        showTotal && <span>共{total}页，当前{current}页</span>
+      }
+        <span>last</span>
+        {
+          this.renderList()
+        }
+        <span>next</span>
       </div>
-    )
+    );
   }
 }
