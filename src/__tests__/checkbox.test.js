7e617a1353976db987f5ef79cf046a876186cf75
AddOndDn <467072280@qq.com>
2019-02-08 19:13:20 +0800

    test: 补充checkbox测试

--- src/__tests__/checkbox.test.js
+++ src/__tests__/checkbox.test.js
@@ -5,25 +5,29 @@ import Checkbox from '../components/checkbox/index'
 
 describe('Checkbox Test', () => {
   const wrapper = shallow(
-    <Checkbox />
+    <Checkbox>灯笼</Checkbox>
   );
 
   it('1. 测试默认class存在', () => {
     expect(wrapper.hasClass('zhui-checkbox-wrapper')).toBe(true);
+    expect(wrapper.find('.zhui-checkbox-input').exists()).toBeTruthy();
+    expect(wrapper.find('.zhui-checkbox-inner').exists()).toBeTruthy();
+    expect(wrapper.find('.zhui-checkbox-text').text()).toBe('灯笼');
   });
 });
 
 describe('Checkbox Group Test', () => {
   const wrapper = shallow(
-    <Checkbox.Group>
-      <Checkbox />
-      <Checkbox />
-      <Checkbox />
+    <Checkbox.Group value={['active1', 'active3']}>
+      <Checkbox value='active1'/>
+      <Checkbox value='active2'/>
+      <Checkbox value='active3'/>
     </Checkbox.Group>
   );
 
   it('1. 测试默认class存在', () => {
     expect(wrapper.hasClass('zhui-checkbox-group')).toBe(true);
+    expect(wrapper.children().length).toBe(3);
   });
 });
 
