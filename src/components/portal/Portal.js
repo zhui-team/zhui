278fd003aad69a570f4048a591c11d65a5786e1e
AddOndDn <467072280@qq.com>
2019-04-23 15:42:13 +0800

    feature: portal组件添加

--- src/components/portal/Portal.js
+++ src/components/portal/Portal.js
@@ -1,35 +1,49 @@
 import React, { PureComponent } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
+import cn from 'astro-classname';
 
 import './index.css';
 
 export default class Portal extends PureComponent {
   static propTypes = {
-    selector: PropTypes.string
+    className: PropTypes.string,
+    selector: PropTypes.string,
+    center: PropTypes.bool,
+    visiable: PropTypes.bool
   }
 
   static defaultProps = {
-    selector: 'body'
-  }
-
-  constructor(props) {
-    super(props);
-
-    this.selector = document.querySelector(props.selector);
+    className: '',
+    selector: 'body',
+    center: false,
+    visiable: true
   }
 
   render() {
     const {
+      className,
       children,
+      center,
+      selector,
+      visiable,
       ...others
     } = this.props;
 
+    const classes = cn('zhui-portal', className, {
+      'zhui-portal-center': center,
+      'zhui-portal-hide': !visiable
+    });
+
+    const selectorContainer = document.querySelector(selector);
+
+    if (!selectorContainer) return null;
+
     return createPortal(
-      <div className="zhui-portal" {...others}>
+      <div className={classes} {...others}>
         {children}
       </div>,
-      this.selector
+      selectorContainer
     );
   }
 }
