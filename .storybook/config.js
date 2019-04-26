3c8a7c654d20d19635d321a6f576f04b6e232d85
AddOndDn <467072280@qq.com>
2019-04-26 21:02:58 +0800

    doc: 文档迁移storybook

---
+++ .storybook/config.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { addParameters, configure, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+
+import '../src/stories/main.scss';
+
+addParameters({
+  options: {
+    name: 'Zhui',
+    showPanel: true,
+    panelPosition: 'bottom'
+  }
+});
+
+const backgroundDecorator = story => (
+  <div style={{ display: 'flex', minHeight: 550, justifyContent: 'center', alignItems: 'center'}}>
+    { story() }
+  </div>
+)
+
+addDecorator(withInfo);
+addDecorator(backgroundDecorator);
+
+function loadStories() {
+  require('../src/stories/button.story.js');
+  require('../src/stories/alert.story.js');
+  require('../src/stories/breadcrumb.story.js');
+  require('../src/stories/card.story.js');
+  require('../src/stories/checkbox.story.js');
+  require('../src/stories/icon.story.js');
+  require('../src/stories/input.story.js');
+  require('../src/stories/loading.story.js');
+  require('../src/stories/notify.story.js');
+  require('../src/stories/portal.story.js');
+  require('../src/stories/progress.story.js');
+  require('../src/stories/steps.story.js');
+  require('../src/stories/radio.story.js');
+  require('../src/stories/tag.story.js');
+}
+
+configure(loadStories, module);
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+// Export a function. Accept the base config as the only param.
+module.exports = async ({ config, mode }) => {
+  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+
+  // Make whatever fine-grained changes you need
+  config.module.rules.push({
+    test: /\.scss$/,
+    loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../'),
+  });
+
+  // Return the altered config
+  return config;
+};
